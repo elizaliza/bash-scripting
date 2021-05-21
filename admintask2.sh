68d8902386fad92505ac9aa4aac07903c3a4038f
elizaliza <vidana_999@mail.ru>
2021-05-21 09:10:15 +0000

    admintask2.sh added

---
+++ admintask2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Author: Eliza
+# Date: 05/21/2021
+
+hostname  # will print hostname 
+uptime #  command shows how long the system has been running. It also displays the current time, the number of logged-in users, and the system load averages for the past 1, 5, and 15 minutes
+df -hTP # will print disk free in h–human-readable,print sizes in power of 1024; T–print-type : print file system type; P–portability,use POSIX output format  use POSIX output format,
+free -h # this command will print memory in human readable format G, M, B
