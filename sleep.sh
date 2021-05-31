4ca6e0f864cb126b8131d071beaa55d167098f82
elizaliza <vidana_999@mail.ru>
2021-05-31 11:06:16 +0000

    sleep.sh added

---
+++ sleep.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo “Wait for 5 seconds”
+sleep 5
+echo “Completed”
+#Sleep Command:
+#When you want to pause the execution of any command for specific period of time then you can use sleep command. You can set the delay amount by seconds (s), minutes (m), hours (h) and days (d). Create a file named ‘sleep_example.sh’ and add the following script. This script will wait for 5 seconds after running.
